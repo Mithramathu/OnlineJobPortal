d1bcaa03e9babc0ecda015cd3f2c30bbc3309f7f
Mithramathu <kmathumithra@gmail.com>
2022-05-04 17:33:02 +0530

    Add project files.

---
+++ OnlineJobPortal.Entity/RequirementDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineJobPortal.Entity
+{
+    public class RequirementDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int JobId { get; set; }
+        public string jobTitle { get; set; }
+        public string Location { get; set; }
+        public string Functional { get; set; }
+        public string TechnicalSkills { get; set; }
+        public string Company { get; set; }
+        public string ContactId { get; set; }
+        public string contactperson { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
